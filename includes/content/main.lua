5178f992b276bd2ce6345ce380fc1dae02334eda
LuisHerranz <luisherranz@gmail.com>
2014-07-16 13:10:38 +0200

    change app_url to wp_url (it's more clear)

--- includes/content/main.lua
+++ includes/content/main.lua
@@ -6,7 +6,7 @@ local content_type = "posts" --. insert content type ( "customcontent" / "posts"
 worona:do_action( "register_service", { service = "content", creator = require("worona.includes.content.services.content") } )
 
 local function downloadContent()
-	worona.content:update( content_type, worona.app_url )
+	worona.content:update( content_type, worona.wp_url )
 end
 
 worona:add_action( "init", downloadContent )
