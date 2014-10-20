df9fc40a22d8004feb9f629bbfd2cec1f4e5d5f0
LuisHerranz <luisherranz@gmail.com>
2014-10-20 22:35:54 +0200

    quick-fix for bug not using https

--- includes/stats/stats.lua
+++ includes/stats/stats.lua
@@ -11,11 +11,8 @@ local function urlencode(str)
 end
 
 local function stats()
-
 	if system.getInfo("environment") == "simulator" and worona.local_options:get("stats") ~= true and worona.stats ~= false then
-		network.request( "http://www.worona.org/stats.php?event=app_execution&wp_url="..urlencode(worona.wp_url).."&app_title="..urlencode(worona.app_title).."&id="..urlencode(system.getInfo("deviceID")), "GET", nil )
-		worona.local_options:set("stats",true)
+		network.request( "https://www.worona.org/stats.php?event=app_execution&wp_url="..urlencode(worona.wp_url).."&app_title="..urlencode(worona.app_title).."&id="..urlencode(system.getInfo("deviceID")), "GET", function() worona.local_options:set("stats",true) end )
 	end
-
 end
 worona:add_action( "init", stats, 10 )
