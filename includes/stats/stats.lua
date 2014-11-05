c7c9bb93c17045f2201d26965eec1bb22894a685
Fernando Moreno <fmorenoper@gmail.com>
2014-11-05 19:18:56 +0100

    modifications for 0.6.3.2

--- includes/stats/stats.lua
+++ includes/stats/stats.lua
@@ -11,7 +11,7 @@ local function urlencode(str)
 end
 
 local function stats()
-	if system.getInfo("environment") == "simulator" and worona.local_options:get("stats") ~= true and worona.stats ~= false then
+	if system.getInfo("environment") == "simulator" and worona.wp_url ~= "http://www.worona.org" and worona.local_options:get("stats") ~= true and worona.stats ~= false then
 		network.request( "https://www.worona.org/stats.php?event=app_execution&wp_url="..urlencode(worona.wp_url).."&app_title="..urlencode(worona.app_title).."&id="..urlencode(system.getInfo("deviceID")), "GET", function() worona.local_options:set("stats",true) end )
 	end
 end
