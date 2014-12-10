c73c4a2da7fdb7f1e5b396c99599cad554500088
Fernando Moreno <fmorenoper@gmail.com>
2014-12-10 13:32:01 +0100

    Modified content to retrieve a certain number of posts.

--- includes/content/content.lua
+++ includes/content/content.lua
@@ -181,7 +181,12 @@ local function newContentService()
 		--. Function arguments compatible with table (worona.content:update( {content_type = "customcontent", url = "testing.turismob.com"} ))
 		if type(options) ~= "table" then
 			content_type = options
-			url = worona.wp_url .. "/wp-json/posts?type=" .. content_type
+
+			if worona.app_number_of_posts == nil or type(worona.app_number_of_posts) ~= "number" then
+				worona.app_number_of_posts = 20
+			end
+
+			url = worona.wp_url .. "/wp-json/posts?filter[posts_per_page]=" .. worona.app_number_of_posts .. "type=" .. content_type
 			
 		else
 			content_type = options.content_type
