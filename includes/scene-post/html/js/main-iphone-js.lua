f2814274da07b00fd7f8549abc1c69da3e79db65
Luis Herranz <luisherranz@gmail.com>
2015-01-20 12:52:43 +0100

    solve bug with loading of the images from the Internet even when they are on the phone

--- includes/scene-post/html/js/main-iphone-js.lua
+++ includes/scene-post/html/js/main-iphone-js.lua
@@ -1,18 +1,22 @@
 return [[
 <script>
-$(function(){
+$(window).load(function(){
 	$('a').each(function(){
 		var href = $(this).attr('href');
-   	$(this).attr('href', 'javascript:');
-   	$(this).click( function(e) {
-   		e.preventDefault();
-          var xobj = new XMLHttpRequest();
-          xobj.open('GET', "http://localhost:1024?url=" + href, true);
-          xobj.send(null);
-   	});
+   		$(this).attr('href', 'javascript:');
+   		$(this).click( function(e) {
+   			e.preventDefault();
+          	var xobj = new XMLHttpRequest();
+          	xobj.open('GET', "http://localhost:1024?url=" + href, true);
+          	xobj.send(null);
+   		});
 	});
 	$('img').each(function(){
-		var web_src = $(this).attr('src');
+		$(this).css({visibility: "hidden", "max-height" : "1px"});
+		$(this).on('load', function() {
+			$(this).css({visibility: "visible", "max-height" : "none"});
+		});
+		var web_src = $(this).attr('data-src');
 		$(this).attr('src', 'http://localhost:1024/img.jpg?img=' + web_src);
 		// if the image is not on the phone, use the web_src
 		var check_one_time = false;
@@ -20,8 +24,6 @@ $(function(){
 			if ( check_one_time === false ) {
 				$(this).attr('src', web_src);
 				check_one_time = true;
-			} else {
-				$(this).css({visibility: "hidden", "max-height" : "1px"});
 			}
 		});
 	});
