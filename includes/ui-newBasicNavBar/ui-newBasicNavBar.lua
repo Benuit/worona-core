806e0679f8eeaab325d32f3bbaadc2381d3f0305
Fernando Moreno <fmorenoper@gmail.com>
2015-03-10 12:14:45 +0100

    added replaceButton service in navbar

--- includes/ui-newBasicNavBar/ui-newBasicNavBar.lua
+++ includes/ui-newBasicNavBar/ui-newBasicNavBar.lua
@@ -5,7 +5,62 @@ local function newBasicNavBar( self, params )
 	local widget = require "widget"
 
 	-- Create the display group
-	local navbar = display.newGroup()
+	local navbar = {}
+
+	navbar.display_group = display.newGroup()
+
+
+	--[[.	
+		replaceButton 
+		
+		Replaces the icon from "side" button for "new_icon"	
+		 	
+		@type: navbar service
+		@date: 03/15
+		@since: 0.6.8
+	
+		@param: side (str): "left" or "right".
+				new_icon: icon table, as in style-default.
+		@return: -
+	
+		@example: basic_navbar:replaceButton ( "right", worona.style:get("icons").is_favorite )
+	]]--
+	function navbar:replaceButton( side, new_icon )
+
+		if new_icon ~= nil then
+			local button_options = {
+				x 			= new_icon.width / 2 - 15,
+				y 			= navbar.attributes.navbar_center_point,
+			    width       = new_icon.width,
+			    height      = new_icon.height,
+			    defaultFile = new_icon.default,
+		        overFile    = new_icon.over,
+		        onRelease   = function() worona:do_action( "navbar_left_button_pushed" ); return true end
+			}
+
+
+			if side == "left" then
+				
+				display.remove( navbar.left_button )
+
+				navbar.left_button_width = new_icon.width
+				navbar.left_button = widget.newButton( button_options )
+				navbar.display_group:insert( navbar.left_button )
+			
+			elseif side == "right" then
+
+				display.remove( navbar.left_button )
+				
+				navbar.right_button_width = new_icon.width
+				navbar.right_button = widget.newButton( button_options )
+				navbar.display_group:insert( navbar.right_button )
+			else
+				worona.log:error('ui-newBasicNavBar/replaceButton: "' .. side .. '" should be "left" or "right". Please change.')
+			end
+
+		end
+
+	end
 
 	-- Get the style
 
@@ -14,7 +69,7 @@ local function newBasicNavBar( self, params )
 	local ios7and8_y      = ( display.topStatusBarContentHeight + style.height ) / 2
 	local ios7and8_height = style.height + display.topStatusBarContentHeight
 
-	local attributes = {
+	navbar.attributes = {
 		navbar_x                 = display.contentWidth / 2,
 		navbar_y                 = worona.style:getSetting( { ios7 = ios7and8_y, ios8 = ios7and8_y, default = display.topStatusBarContentHeight + style.height / 2 } ),
 		navbar_center_point      = display.topStatusBarContentHeight + style.height / 2,
@@ -30,68 +85,68 @@ local function newBasicNavBar( self, params )
 
 	-- Start the creation
 
-	local background = display.newRect( attributes.navbar_x, attributes.navbar_y, attributes.navbar_width, attributes.navbar_height )
-	background:setFillColor( attributes.background_color[1], attributes.background_color[2], attributes.background_color[3], attributes.background_color[4] )
-	navbar:insert(background)
+	local background = display.newRect( navbar.attributes.navbar_x, navbar.attributes.navbar_y, navbar.attributes.navbar_width, navbar.attributes.navbar_height )
+	background:setFillColor( navbar.attributes.background_color[1], navbar.attributes.background_color[2], navbar.attributes.background_color[3], navbar.attributes.background_color[4] )
+	navbar.display_group:insert(background)
 
-	local stroke = display.newRect( attributes.navbar_x, attributes.navbar_center_point + ( style.height + attributes.background_stroke_height ) / 2, attributes.navbar_width, attributes.background_stroke_height )
-	stroke:setFillColor( attributes.background_stroke_color[1], attributes.background_stroke_color[2], attributes.background_stroke_color[3], attributes.background_stroke_color[4] )
-	navbar:insert(stroke)
+	local stroke = display.newRect( navbar.attributes.navbar_x, navbar.attributes.navbar_center_point + ( style.height + navbar.attributes.background_stroke_height ) / 2, navbar.attributes.navbar_width, navbar.attributes.background_stroke_height )
+	stroke:setFillColor( navbar.attributes.background_stroke_color[1], navbar.attributes.background_stroke_color[2], navbar.attributes.background_stroke_color[3], navbar.attributes.background_stroke_color[4] )
+	navbar.display_group:insert(stroke)
 
-	local left_button_width = 0
+	navbar.left_button_width = 0
 	if params.left_button_icon ~= nil then
 		local left_button_options = {
 			x 			= params.left_button_icon.width / 2 - 15,
-			y 			= attributes.navbar_center_point,
+			y 			= navbar.attributes.navbar_center_point,
 		    width       = params.left_button_icon.width,
 		    height      = params.left_button_icon.height,
 		    defaultFile = params.left_button_icon.default,
 	        overFile    = params.left_button_icon.over,
 	        onRelease   = function() worona:do_action( "navbar_left_button_pushed" ); return true end
 		}
-		left_button_width = params.left_button_icon.width
-		local left_button = widget.newButton( left_button_options )
-		navbar:insert( left_button )
+		navbar.left_button_width = params.left_button_icon.width
+		navbar.left_button = widget.newButton( left_button_options )
+		navbar.display_group:insert( navbar.left_button )
 	end
 
-	local right_button_width = 0
+	navbar.right_button_width = 0
 	if params.right_button_icon ~= nil then
 		local right_button_options = {
 			x 			= display.contentWidth - ( params.right_button_icon.width / 2 ) + 15,
-			y 			= attributes.navbar_center_point,
+			y 			= navbar.attributes.navbar_center_point,
 		    width       = params.right_button_icon.width,
 			height      = params.right_button_icon.height,
 		    defaultFile = params.right_button_icon.default,
 	        overFile    = params.right_button_icon.over,
 	        onRelease   = function() worona:do_action( "navbar_right_button_pushed" ); return true end
 		}
-		right_button_width = params.left_button_icon.width
-		local right_button = widget.newButton( right_button_options )
-		navbar:insert( right_button )
+		navbar.right_button_width = params.left_button_icon.width
+		navbar.right_button = widget.newButton( right_button_options )
+		navbar.display_group:insert( navbar.right_button )
 
 	end
 
 	local text_options = {
-		parent   = navbar,
+		parent   = navbar.display_group,
 		text     = params.text or "nothing",
-		x        = attributes.navbar_x,
-		y        = attributes.navbar_center_point,
-		fontSize = attributes.text_size
+		x        = navbar.attributes.navbar_x,
+		y        = navbar.attributes.navbar_center_point,
+		fontSize = navbar.attributes.text_size
 	}
 
 	local text = display.newText( text_options )
-	text:setFillColor( attributes.text_color[1], attributes.text_color[2], attributes.text_color[3], attributes.text_color[4] )
+	text:setFillColor( navbar.attributes.text_color[1], navbar.attributes.text_color[2], navbar.attributes.text_color[3], navbar.attributes.text_color[4] )
 
 	--. If text is too wide, cut it and add "..."
-	local nabvar_available_space = display.contentWidth - right_button_width - left_button_width - 10
-	if text.width > nabvar_available_space then
+	local navbar_available_space = display.contentWidth - navbar.right_button_width - navbar.left_button_width - 10
+	if text.width > navbar_available_space then
 		--. Calculate the width of a character:
 		text.text = "o"
 		local character_width = text.width
 		print(character_width)
 
-		--. Calculate how many character can be placed in the nabvar:
-		local characters_number = math.floor(nabvar_available_space / character_width)
+		--. Calculate how many character can be placed in the navbar:
+		local characters_number = math.floor(navbar_available_space / character_width)
 
 		--. Insert text with new length
 		text.text = text_options.text:sub(1, characters_number - 3) .. "..."
@@ -99,7 +154,7 @@ local function newBasicNavBar( self, params )
 
 	-- Insert navbar on the parent group
 	if params ~= nil and params.parent ~= nil then
-		params.parent:insert( navbar )
+		params.parent:insert( navbar.display_group )
 	end
 
 	-- Return the object
