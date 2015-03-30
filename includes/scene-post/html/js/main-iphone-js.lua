37107a6e7f9a9dee8c845004027e7accaf761cc6
Luis Herranz <luisherranz@gmail.com>
2015-03-30 15:40:56 +0200

    fix for faster webview loading

--- includes/scene-post/html/js/main-iphone-js.lua
+++ includes/scene-post/html/js/main-iphone-js.lua
@@ -11,22 +11,24 @@ $(window).load(function(){
           	xobj.send(null);
    		});
 	});
-	$('img').each(function(){
-		$(this).css({visibility: "hidden", "max-height" : "1px"});
-		$(this).on('load', function() {
-			$(this).css({visibility: "visible", "max-height" : "none"});
+	setTimeout(function(){
+		$('img').each(function(){
+			$(this).css({visibility: "hidden", "max-height" : "1px"});
+			$(this).on('load', function() {
+				$(this).css({visibility: "visible", "max-height" : "none"});
+			});
+			var web_src = $(this).attr('data-src');
+			$(this).attr('src', 'http://localhost:1024/img.jpg?img=' + web_src);
+			// if the image is not on the phone, use the web_src
+			var check_one_time = false;
+			$(this).on('error', function () {
+				if ( check_one_time === false ) {
+					$(this).attr('src', web_src);
+					check_one_time = true;
+				}
+			});
 		});
-		var web_src = $(this).attr('data-src');
-		$(this).attr('src', 'http://localhost:1024/img.jpg?img=' + web_src);
-		// if the image is not on the phone, use the web_src
-		var check_one_time = false;
-		$(this).on('error', function () {
-			if ( check_one_time === false ) {
-				$(this).attr('src', web_src);
-				check_one_time = true;
-			}
-		});
-	});
+	},1);
 });
 </script>
 ]]
